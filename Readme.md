be40b8786ebfb2e09d36967c08da5cdbd0f57695
kevin.henderson <kevin.henderson@virtualinstruments.com>
2015-10-23 16:52:49 -0400

    Fix the mail posting for angular

--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 # kevhender.net
 
-This is the repository for my personal website, kevhender.net. The `"sencha"` folder contains the 
-ExtJS version of the website.
+This is the repository for my personal website, kevhender.net. The "sencha" folder contains the 
+ExtJS version of the website, and the `angular` folder is the AngularJS/Bootstrap version.
 
 See the site in action at [kevhender.net](http://www.kevhender.net).
 
--- angular/js/Contact.js
+++ angular/js/Contact.js
@@ -12,7 +12,8 @@
                 $http({
                     url: '../sendMail.php',
                     method: 'POST',
-                    params: contact
+                    data: $.param(contact),
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
                 }).then(
                     function(resp) {
                         $scope.contact = {};
@@ -20,7 +21,7 @@
                     },
                     function(resp) {
                         $scope.contact = {};
-                        $window.alert('Thank you for your submission, I will get back to you as soon as I can!.');
+                        $window.alert('Sorry, there was an error submitting your message.  Please try again later.');
                     }
                 );
             }
