1cea87fcf42723d839b22af93f3805c31bf9e905
kevhender <kevhender@gmail.com>
2019-03-25 07:36:17 -0400

    Production build

--- server-debug.js
+++ server-debug.js
@@ -5,20 +5,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { sendEmail } = require('./src/contact/sendEmail');
 const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 const webpack = require('webpack');
-const webpackConfig = require('./webpack.production.config.js');
+const webpackConfig = require('./webpack.config.js');
 
 const app = express();
 
 const compiler = webpack(webpackConfig);
 
 app.use(webpackDevMiddleware(compiler, {
+  hot: true,
   filename: 'bundle.js',
   publicPath: '/',
   stats: {
     colors: true,
   },
   historyApiFallback: true,
+}));
+
+app.use(webpackHotMiddleware(compiler, {
+  log: console.log,
+  path: '/__webpack_hmr',
+  heartbeat: 10 * 1000,
 }));
 
 app.use(express.static(`${__dirname}/www`));
